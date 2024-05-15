7e1ae0c72422b19eb75af4e9fa95cbc4a656ec01
Hai Nguyen Quang <hai@caliber.build>
2024-05-15 22:58:07 +0700

    feat: derive (De)Serialize for PlainAccount

--- crates/revm/src/db/states/plain_account.rs
+++ crates/revm/src/db/states/plain_account.rs
@@ -2,6 +2,7 @@ use revm_interpreter::primitives::{AccountInfo, HashMap, StorageSlot, U256};
 
 // TODO rename this to BundleAccount. As for the block level we have original state.
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct PlainAccount {
     pub info: AccountInfo,
     pub storage: PlainStorage,
